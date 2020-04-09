2b635b0759daf5b6836c672dd24eba94440a61c5
DuongTung <59309452+DuongTung2003@users.noreply.github.com>
2020-04-09 20:29:01 +0700

    Update Main

--- Main.py
+++ Main.py
@@ -6,6 +6,9 @@ from datetime import datetime
 import os
 import time
 import openpyxl
+from fbchat import log, Client
+from  fbchat.models import *
+from threading import Thread
 try:
     os.mkdir("LogFiles/")
 except :
@@ -43,6 +46,7 @@ class Person:
         self.Lop = Lop
         self.IMG = IMG
 class DataBase:
+    client = None
     Face_encodings = []
     Face_STTs = []
     Thong_tin_hs = []
@@ -90,10 +94,11 @@ class Nhan_dang():
                     best_match_index = np.argmin(face_distances)
                     if matches[best_match_index]:
                         STT = DataBase.Face_STTs[best_match_index]
-                        DataBase.Co_mat.append(DataBase.Thong_tin_hs[best_match_index])
+                        if DataBase.Thong_tin_hs[best_match_index] not in DataBase.Co_mat:
+                            DataBase.Co_mat.append(DataBase.Thong_tin_hs[best_match_index])
                     self.face_STTs.append(STT)
                     Console.Log("ID hoc sinh: ",best_match_index,"Thong tin: ",DataBase.Thong_tin_hs[best_match_index].Ten,DataBase.Thong_tin_hs[best_match_index].Lop)
-                if self.face_encodings and frame_count == 0 :
+                if self.face_encodings and frame_count == 0 and STT == "Unknown":
                         for (top, right, bottom, left), STT in zip(self.face_locations, self.face_STTs):
                             top *= 4
                             right *= 4
@@ -116,12 +121,94 @@ class Nhan_dang():
            
 
         video_capture.release()
+        
+
+class React(Client):
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+     if author_id != self.uid and  (str( message_object.text)[0] == "1" or message_object.text == "Toàn trường") :
+        thong_bao = ""
+        # We can only kick people from group chats, so no need to try if it's a user chat
+        self.reactToMessage(message_object.uid, MessageReaction.HEART)
+        DataBase.client.setTypingStatus(TypingStatus.TYPING , thread_id=thread_id, thread_type=thread_type)
+        
+        if message_object.text == "Toàn trường" and thread_type == ThreadType.GROUP :
+            self.reactToMessage(message_object.uid, MessageReaction.HEART)
+            Si_so_lop_co_mat = []
+            Si_so_lop = []
+            Si_so_truong = len(DataBase.Thong_tin_hs)
+            Si_so_truong_dd = len(DataBase.Co_mat)
+            print(DataBase.Co_mat)
+            for Hs in DataBase.Thong_tin_hs:
+               Si_so_lop.append(Hs.Lop)
+            for Hs_co_mat in DataBase.Co_mat:
+                Si_so_lop_co_mat.append(Hs_co_mat.Lop)
+            cac_lop = [ [l, Si_so_lop.count(l)] for l in set(Si_so_lop)]
+            cac_lop_da_dd = [ [l, Si_so_lop_co_mat.count(l)] for l in set(Si_so_lop_co_mat)]
+            thong_bao = "Sĩ số toàn trường: "
+            thong_bao += str(Si_so_truong_dd)+"/"+ str(Si_so_truong)+ "\n Theo từng lớp: \n"
+            print(cac_lop)
+            for Lop in cac_lop :
+                for Lop_da_dd in cac_lop_da_dd:
+                    if Lop_da_dd[0] == Lop[0]:
+                        thong_bao += "Lớp "+ str(Lop[0])+": " +str(Lop_da_dd[1])+"/"+str(Lop[1])+"\n"
+                        break
+                    
+            Console.Log(thong_bao)
+            DataBase.client.setTypingStatus(TypingStatus.STOPPED , thread_id=thread_id, thread_type=thread_type)
+            DataBase.client.send(Message(text=thong_bao), thread_id=thread_id, thread_type=thread_type)
+        
+        else:
+          if str( message_object.text)[0] == "1":
+           Si_so_lop =[]
+           for Hs in DataBase.Thong_tin_hs:
+              Si_so_lop.append(Hs.Lop)
+           if  str( message_object.text) in Si_so_lop:
+            # Sends the data to the inherited onMessage, so that we can still see when a message is recieved
+            Lop = str( message_object.text)
+            
+            Lop_vang = []
+            Lop_tong = []
+            for Hs in DataBase.Thong_tin_hs:
+                if Hs.Lop == Lop:
+                    Lop_tong.append(Hs)
+                    if Hs not in DataBase.Co_mat:
+                        Lop_vang.append(Hs)
+            thong_bao = "Sĩ số lớp "+Lop+": "+str(len(Lop_tong) - len(Lop_vang))+"/"+str(len(Lop_tong))+"\n"
+            thong_bao += "Vắng:\n" if len(Lop_vang) > 0 else "Lớp đủ"
+            for i in Lop_vang:
+                thong_bao +=i.STT + " " + i.Ten +"\n"
+            Console.Log(thong_bao)
+            DataBase.client.setTypingStatus(TypingStatus.STOPPED , thread_id=thread_id, thread_type=thread_type)
+            DataBase.client.send(Message(text=thong_bao), thread_id=thread_id, thread_type=thread_type)
+
+
+
+
+            super(React, self).onMessage(
+                author_id=author_id,
+                message_object=message_object,
+                thread_id=thread_id,
+                thread_type=thread_type,
+                **kwargs
+            )
+           else:
+                DataBase.client.send(Message(text="Lớp không tồn tại !"), thread_id=thread_id, thread_type=thread_type)
+
+
+        
+
+def Start_FB():
+    DataBase.client = React('duongdoantung2k3@gmail.com', 'tung2003')
+    DataBase.client.listen()
 class Main():
      def __init__(self):
         self.files =  os.listdir("./IMG_DataBase/")
         self.en_file = os.listdir("./Face_data/")
         self.ReadExcel()
+        FB = Thread(target=Start_FB)
+        FB.start()
         Nhan_dang()
+
      def New_entry(self,Thong_tin_hs,Face_path):
 
         if (Thong_tin_hs.Lop+"_"+Thong_tin_hs.STT+".data") in self.en_file:
@@ -169,9 +256,10 @@ class Main():
                     if (ws.title+"_"+str(Row[0].value)+".png" in self.files) or  (ws.title+"_"+str(Row[0].value)+".jpg" in self.files ):
                         Hoc_sinh = Person(str(Row[0].value),ws.title,"./IMG_DataBase/"+((ws.title+"_"+str(Row[0].value)+".png") if ws.title+"_"+str(Row[0].value)+".png" in self.files else ws.title+"_"+str(Row[0].value)+".jpg" ) ,str(Row[1].value))
                         self.New_entry(Hoc_sinh,Hoc_sinh.IMG)
-
+                    else:
+                        self.missing_data.append(Person(str(Row[0].value),ws.title,"",str(Row[1].value)))
+                        Console.Warning("Missing data: ",str(Row[1].value),ws.title)
                 else:
-                   self.missing_data.append(Person(str(Row[0].value),ws.title,"",str(Row[1].value)))
                    del Row
             Console.Log("DataBase: ",len(DataBase.Thong_tin_hs))
 Main()
