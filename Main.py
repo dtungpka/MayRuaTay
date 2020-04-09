7e3a646ee651373c9ff7a334de9e983dde57d8f1
DuongTung <59309452+DuongTung2003@users.noreply.github.com>
2020-04-09 14:34:47 +0700

    Added Excel support

--- Main.py
+++ Main.py
@@ -5,48 +5,59 @@ import logging
 from datetime import datetime
 import os
 import time
+import openpyxl
 try:
     os.mkdir("LogFiles/")
 except :
     pass
 format = "%(asctime)s: %(message)s"
 logname =  "./LogFiles/Log_"+str(datetime.now().month) +"_"+ str(datetime.now().day)+"_"+ str(datetime.now().hour)+"_"+ str(datetime.now().minute)+".log"
-logging.basicConfig(filename=logname,format=format, level=logging.DEBUG,datefmt="%H:%M:%S")
+logging.basicConfig(filename=logname,format=format, level=logging.INFO,datefmt="%H:%M:%S")
 class Console():
     def Log(*arg):
-        logging.info("INFO ",arg)
-        print(arg)
+        msg = ""
+        for inf in arg:
+            msg += str(inf) + " "
+        logging.info("INFO "+msg)
+        print("INFO "+msg)
     def Warning(*arg):
-        logging.warning("WARNING ",arg)
-        print(arg)
-    def error(*arg):
-        logging.error("ERROR ",arg)
-        print(arg)
-
-class DataBase():
+        msg = ""
+        for inf in arg:
+            msg += str(inf) + " "
+        logging.warning("WARNING "+msg)
+        print("WARNING "+msg)
+    def Error(*arg):
+        msg = ""
+        for inf in arg:
+            msg += str(inf) + " "
+        logging.error("ERROR "+msg)
+        print("ERROR "+msg)
+class Person:
+    Ten = ""
+    Lop = ""
+    IMG = ""
+    STT = ""
+    def __init__(self,STT, Lop,IMG,Ten):
+        self.STT = STT
+        self.Ten = Ten
+        self.Lop = Lop
+        self.IMG = IMG
+class DataBase:
     Face_encodings = []
-    Face_names = []
+    Face_STTs = []
+    Thong_tin_hs = []
+    Co_mat = []
 
-class DataBase_Handler():
-    def New_entry(Name,Face_path):
-        image = face_recognition.load_image_file(Face_path)
-        face_encoding = face_recognition.face_encodings(image)[0]
-        DataBase.Face_names.append(Name)
-        DataBase.Face_encodings.append(face_encoding)
-        Console.Log("New entry: ",Name," _ ",Face_path)
-    def Delete_entry(Name):
-        pass
 
     
     
 class Nhan_dang():
     def __init__(self):
-        print("Starting")
+        Console.Log("Starting")
         self.face_locations = []
         self.face_encodings = []
-        self.face_names = []
+        self.face_STTs = []
         self.Running = True
-        DataBase_Handler.New_entry("Dummy","./examples/obama.jpg")
         self.Bat_dau()
         
     def Bat_dau(self):
@@ -70,19 +81,20 @@ class Nhan_dang():
                 if not frame_count == 0 :
                     frame_count = 0 if time.time() - start_cooldown > 3 else frame_count - 1
                 
-                self.face_names = []
+                self.face_STTs = []
                 for face_encoding in self.face_encodings:
                     matches = face_recognition.compare_faces(DataBase.Face_encodings, face_encoding)
-                    name = "Unknown"
+                    STT = "Unknown"
                     face_distances = face_recognition.face_distance(DataBase.Face_encodings, face_encoding)
                     print("Dist",face_distances)
                     best_match_index = np.argmin(face_distances)
                     if matches[best_match_index]:
-                        name = DataBase.Face_names[best_match_index]
-                    self.face_names.append(name)
-                    Console.Log("Person: ",self.face_names,"Dist",face_distances)
-                if self.face_encodings :
-                        for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                        STT = DataBase.Face_STTs[best_match_index]
+                        DataBase.Co_mat.append(DataBase.Thong_tin_hs[best_match_index])
+                    self.face_STTs.append(STT)
+                    Console.Log("ID hoc sinh: ",best_match_index,"Thong tin: ",DataBase.Thong_tin_hs[best_match_index].Ten,DataBase.Thong_tin_hs[best_match_index].Lop)
+                if self.face_encodings and frame_count == 0 :
+                        for (top, right, bottom, left), STT in zip(self.face_locations, self.face_STTs):
                             top *= 4
                             right *= 4
                             bottom *= 4
@@ -91,11 +103,10 @@ class Nhan_dang():
                     
                             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
                             font = cv2.FONT_HERSHEY_DUPLEX
-                            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-                    
-                        # Display the resulting image
+                            cv2.putText(frame, STT, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                   
                         #cv2.imshow('Video', frame)
-                        cv2.imwrite("./Capture/Frame"+str(count)+".png",frame)
+                        cv2.imwrite("./Capture/"+STT+str(count)+".png",frame)
                         count +=1
                         frame_count = 50
                         start_cooldown = time.time()
@@ -105,14 +116,62 @@ class Nhan_dang():
            
 
         video_capture.release()
-        cv2.destroyAllWindows()
 class Main():
      def __init__(self):
-        files =  os.listdir("./IMG_DataBase/")
-        for file in files:
-            data = file.split(".")[0]
-            Lop = data.split("_")[0]
-            STT = data.split("_")[1]
-            DataBase_Handler.New_entry(STT+"_"+Lop,"./IMG_DataBase/"+ file)
-            Nhan_dang()
+        self.files =  os.listdir("./IMG_DataBase/")
+        self.en_file = os.listdir("./Face_data/")
+        self.ReadExcel()
+        Nhan_dang()
+     def New_entry(self,Thong_tin_hs,Face_path):
+
+        if (Thong_tin_hs.Lop+"_"+Thong_tin_hs.STT+".data") in self.en_file:
+            face_encoding = self.Read_en(Thong_tin_hs.Lop+"_"+Thong_tin_hs.STT)
+        else:
+            image = face_recognition.load_image_file(Face_path)
+            face_encoding = face_recognition.face_encodings(image)[0]
+            self.Write_en(face_encoding,Thong_tin_hs.Lop+"_"+Thong_tin_hs.STT)
+        print(face_encoding)
+        print(type(face_encoding[0]),len(face_encoding))
+        DataBase.Face_STTs.append(Thong_tin_hs.Ten)
+        DataBase.Face_encodings.append(face_encoding)
+        DataBase.Thong_tin_hs.append(Thong_tin_hs)
+        Console.Log("New entry: ",Thong_tin_hs.Ten," ",Face_path)
+     def Read_en(self,file_name):
+          with open("./Face_data/"+file_name+".data","r") as File:
+             data_str = File.read().split(" ")
+             data_list = []
+             for i in data_str:
+                 data_list.append(float(i))
+             data = np.array(data_list,np.float64)
+             File.close()
+             Console.Log("Read complete")
+             return data
+     def Write_en(self,data,file_name):
+         
+         with open("./Face_data/"+file_name+".data","w+") as File:
+             string = ""
+             for x in data:
+                string += str(x) + " "
+             string = string[:-1]
+             File.write(string)
+             File.close()
+             Console.Log("Write complete")
+     def ReadExcel(self):
+        wb = openpyxl.load_workbook('Thong_tin_hs.xlsx')
+        self.missing_data = []
+        for ws in wb:
+            Console.Log("Lop",ws.title)
+            Colunm = ws['A3':'B100']
+            Console.Log("Main: ",self.files)
+            for Row in Colunm:
+                if Row[0].value != None:
+                    
+                    if (ws.title+"_"+str(Row[0].value)+".png" in self.files) or  (ws.title+"_"+str(Row[0].value)+".jpg" in self.files ):
+                        Hoc_sinh = Person(str(Row[0].value),ws.title,"./IMG_DataBase/"+((ws.title+"_"+str(Row[0].value)+".png") if ws.title+"_"+str(Row[0].value)+".png" in self.files else ws.title+"_"+str(Row[0].value)+".jpg" ) ,str(Row[1].value))
+                        self.New_entry(Hoc_sinh,Hoc_sinh.IMG)
+
+                else:
+                   self.missing_data.append(Person(str(Row[0].value),ws.title,"",str(Row[1].value)))
+                   del Row
+            Console.Log("DataBase: ",len(DataBase.Thong_tin_hs))
 Main()
